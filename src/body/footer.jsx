35f03d18f8e205c038c48e5a8fa6636b293b0f47
iremfocali <irem.focali@hotmail.com>
2022-02-15 01:16:04 +0300

    react imported to footer file

---
+++ src/body/footer.jsx
@@ -0,0 +1,24 @@
+import Defines from '../context/defines';
+import React, { useContext, useState, useEffect } from "react";
+
+
+const Footer = () => {
+
+    let { defines, setDefines } = useContext(Defines);
+    
+    return (
+        <>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                     
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default Footer;
