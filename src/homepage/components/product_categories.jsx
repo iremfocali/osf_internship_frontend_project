fed7fb71cc647435a486c3a7c2495b5a8a8a9881
iremfocali <irem.focali@hotmail.com>
2022-02-15 01:24:50 +0300

    breadcrumb imported to prod_cat comp. in services

---
+++ src/homepage/components/product_categories.jsx
@@ -0,0 +1,40 @@
+import Defines from '../../context/defines';
+import Breadcrumb from "../../body/breadcrumb";
+import React, { useContext, useState, useEffect } from "react";
+
+
+const productCategories = () => {
+
+    let { defines, setDefines } = useContext(Defines);
+
+    let [services, setServices] = useState({title: ''})
+
+
+    useEffect(() => {
+        let breadcrumb = [{ name: "Homepage", url: "/homepage", active: true }, { name: "Services", url: "/services", active: true }, { name: "Product Categories", url: "/product_categories", active: false }];
+
+        setDefines(prev => {
+            const newDefines = { ...prev }
+            newDefines.breadcrumb = breadcrumb;
+            return newDefines
+        })
+
+    }, [setDefines])
+
+    return (
+        <>
+            <Breadcrumb />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                     
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default productCategories;
