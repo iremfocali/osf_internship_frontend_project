2ce518026cabc60d262fcec8c1bd886c8f61b9d3
iremfocali <irem.focali@hotmail.com>
2022-02-15 00:57:33 +0300

    react imported to header file

---
+++ src/body/404.jsx
@@ -0,0 +1,18 @@
+import React, { useContext } from "react";
+import Defines from '../context/defines';
+
+// COMPONENTS 
+
+const error_404 = props => {
+
+    // STATES
+    let { defines } = useContext(Defines);
+
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default error_404;
