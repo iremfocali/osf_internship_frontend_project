ac35b42125e860d0a2dcc6b5f9413cbde9790c63
iremfocali <irem.focali@hotmail.com>
2022-02-15 18:50:08 +0300

    fr component of EN category

---
+++ src/en/components/fr.jsx
@@ -0,0 +1,40 @@
+import Defines from '../../context/defines';
+import Breadcrumb from "../../body/breadcrumb";
+import React, { useContext, useState, useEffect } from "react";
+
+
+const fr = () => {
+
+    let { defines, setDefines } = useContext(Defines);
+
+    let [services, setServices] = useState({title: ''})
+
+
+    useEffect(() => {
+        let breadcrumb = [{ name: "Homepage", url: "/homepage", active: true }, { name: "En", url: "/en", active: true }, { name: "Fr", url: "/fr", active: false }];
+
+        setDefines(prev => {
+            const newDefines = { ...prev }
+            newDefines.breadcrumb = breadcrumb;
+            return newDefines
+        })
+
+    }, [setDefines])
+
+    return (
+        <>
+            <Breadcrumb />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                     
+                    </div>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default fr;
