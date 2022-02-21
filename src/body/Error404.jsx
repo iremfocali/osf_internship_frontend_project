566160c73cc18487487aade0626cd72b7ebd0c76
iremfocali <irem.focali@hotmail.com>
2022-02-22 00:50:49 +0300

    error404 page designed and added

---
+++ src/body/Error404.jsx
@@ -0,0 +1,33 @@
+import React, { useContext } from "react";
+import Defines from '../context/defines';
+
+// COMPONENTS 
+
+const Error404 = props => {
+
+    // STATES
+    let { defines } = useContext(Defines);
+
+    return (
+        <>
+    <div className="error_page">
+        <div className="jumbotron">
+            <div>
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb style_breadcrumb">
+                      <li className="breadcrumb-item" ><a href="#">Home</a></li>
+                      <li className="breadcrumb-item active"  aria-current="page">404</li>
+                    </ol>
+                  </nav>
+            </div>
+            <hr />
+            <div className="error_image">
+                <img src="./not-found-1280 - Kopya.jpg" alt="" style={{borderRadius:"8px"}} />
+            </div>
+          </div>
+      </div>
+        </>
+    );
+};
+
+export default Error404;
